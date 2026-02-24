adafe4de0540852f8c5c3dadbd05d38e673b71c3
Trần Văn Khôi <strong.tran@vietis.com.vn>
2026-02-24 11:21:23 +0700

    fix(deploy): switch to static SPA build for S3 hosting

---
+++ src/entry-client.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { RouterProvider } from "@tanstack/react-router";
+import { getRouter } from "./router";
+
+const router = getRouter();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
